a55a1ede9b20ff92c79960318a2361b825415ee3
Ralph Subrio <ralphsubrio@MacBook-Pro.local>
2020-08-16 22:16:32 +0800

    deploy static files

--- configs/settings.py
+++ configs/settings.py
@@ -113,9 +113,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = ''
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/')
+]
 
 if os.environ['ENVIRONMENT'] == 'production':
     from .other_settings.production import *
